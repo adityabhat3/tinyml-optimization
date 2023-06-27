6a7f569a1f08f1caa506298fd4aca82eb69e6501
adityabhat3 <adityabhat2003@yahoo.com>
2023-06-27 14:49:53 +0530

    rgb565 to rgb888

--- capture_raw_bytes.ino
+++ capture_raw_bytes.ino
@@ -4,6 +4,13 @@ int bytes_per_frame;
 
 byte data[320*240*2];
 
+void rgb565_rgb888(uint8_t* in, uint8_t* out) {
+  uint16_t p = (in[0] << 8) | in[1];
+  out[0] = ((p >> 11) & 0x1f) << 3;
+  out[1] = ((p >> 5) & 0x3f) << 2;
+  out[2] = (p & 0x1f) << 3;
+}
+
 void setup() {
   Serial.begin(115200); //baud rate
   
@@ -18,6 +25,9 @@ void setup() {
     while (1);
   }
   
+  Camera.testPattern(); //comment to stop test
+
+  
   bytes_per_frame = Camera.width() * Camera.height() * Camera.bytesPerPixel();
   
 }
@@ -26,11 +36,19 @@ void loop() {
   bool button_clicked = readShieldButton();
   
   if(button_clicked){
-    Serial.println("Reading frame");
-    Serial.println();
-
     Camera.readFrame(data);
-    Serial.write(data, bytes_per_frame);
-    Serial.println("Image Captured");
+    uint8_t rgb888[3];
+    Serial.println("<image>");    
+    Serial.println(Camera.width());
+    Serial.println(Camera.height());
+    const int bytes_per_pixel = Camera.bytesPerPixel();
+    
+    for(int i = 0; i < bytes_per_frame; i+=bytes_per_pixel) {
+      rgb565_rgb888(&data[i], &rgb888[0]);
+      Serial.println(rgb888[0]);
+      Serial.println(rgb888[1]);
+      Serial.println(rgb888[2]);
+    }
+    Serial.println("</image>");
   }
 }
